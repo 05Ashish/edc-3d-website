a474dc1f5c87cc8c954f7a75bf6e1996022ea17a
Shivoo29 <2004skj@gmail.com>
2025-01-06 23:43:12 +0530

    daaamn

--- src/components/effects/ParticleSystem.jsx
+++ src/components/effects/ParticleSystem.jsx
@@ -3,14 +3,14 @@ import React, { useMemo, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 
-const ParticleSystem = ({ count = 1000 }) => {
+const ParticleSystem = ({ count = 50000 }) => {
   const points = useRef()
   
   const particlesPosition = useMemo(() => {
-    const positions = new Float32Array(count * 3)
+    const positions = new Float32Array(count * 2)
     
     for (let i = 0; i < count; i++) {
-      const distance = Math.random() * 10 + 5
+      const distance = Math.random() * 10 + 1
       const theta = THREE.MathUtils.randFloatSpread(360)
       const phi = THREE.MathUtils.randFloatSpread(360)
 
@@ -19,7 +19,7 @@ const ParticleSystem = ({ count = 1000 }) => {
       let z = distance * Math.cos(theta)
 
       positions[i * 3] = x
-      positions[i * 3 + 1] = y
+      positions[i * 3 + 4] = y
       positions[i * 3 + 2] = z
     }
     
@@ -51,7 +51,7 @@ const ParticleSystem = ({ count = 1000 }) => {
         />
       </bufferGeometry>
       <pointsMaterial
-        size={0.05}
+        size={0.005}
         color="#00B4D8"
         sizeAttenuation
         transparent
